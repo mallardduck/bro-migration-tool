65eee782844317e87a8f397a6aa95c38bd6084f3
Dan Pock <self@danpock.me>
2024-05-17 12:40:01 -0400

    refactor logic

--- pkg/migrate/migrate.go
+++ pkg/migrate/migrate.go
@@ -1,27 +1,40 @@
 package migrate
 
 import (
+	"fmt"
+	"github.com/sirupsen/logrus"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/apis/meta/v1/unstructured"
 	"regexp"
+	"strings"
 )
 
 func K3sRancherToRke2Rancher(k3sLocalCluster unstructured.Unstructured) unstructured.Unstructured {
 	// Replace "^k3s" with "^rke2" on both key and values
-	res := findAndReplaceValue(k3sLocalCluster, `^k3s`, "rke2")
-	res = findAndReplaceKey(res, `^f:k3s`, "f:rke2")
-	res = findAndReplaceValue(res, `k3s([0-9]*)$`, "rke2r$1")
+	res := findAndReplaceLabelsValue(k3sLocalCluster, `^k3s$`, "rke2")
+	res = findAndReplaceAnnotationsValue(res, `k3s([0-9]*)$`, "rke2r$1")
+	res = findAndReplaceStatusValue(k3sLocalCluster, `^k3s$`, "rke2")
+	res = findAndReplaceValueOn(res, "spec.k3sConfig", `k3s([0-9]*)$`, "rke2r$1")
+	res = findAndReplaceKeyOn(res, "spec.k3sConfig", `^k3s`, "rke2")
+	res = findAndReplaceKeyOn(res, "spec", `^k3s`, "rke2")
+	res = findAndReplaceManagedFieldsKey(res, `f:k3s`, "f:rke2")
+
 	return res
 }
 
 func Rke2RancherToK3sRancher(k3sLocalCluster unstructured.Unstructured) unstructured.Unstructured {
-	// Replace "^k3s" with "^rke2" on both key and values
-	res := findAndReplaceValue(k3sLocalCluster, `^rke2`, "k3s")
-	res = findAndReplaceKey(res, `^f:rke2`, "f:k3s")
-	res = findAndReplaceValue(res, `rke2r([0-9]*)$`, "k3s$1")
+	res := findAndReplaceLabelsValue(k3sLocalCluster, `^rke2$`, "k3s")
+	res = findAndReplaceAnnotationsValue(res, `rke2r([0-9]*)$`, "k3s$1")
+	res = findAndReplaceStatusValue(k3sLocalCluster, `^rke2$`, "k3s")
+	res = findAndReplaceValueOn(res, "spec.rke2Config", `rke2r([0-9]*)$`, "k3s$1")
+	res = findAndReplaceKeyOn(res, "spec.rke2Config", `^rke2`, "k3s")
+	res = findAndReplaceKeyOn(res, "spec", `^rke2`, "k3s")
+	res = findAndReplaceManagedFieldsKey(res, `f:rke2`, "f:k3s")
+
 	return res
 }
 
-func findAndReplaceValue(data unstructured.Unstructured, targetKey string, replacement string) unstructured.Unstructured {
+func findAndReplaceLabelsValue(data unstructured.Unstructured, targetKey string, replacement string) unstructured.Unstructured {
 	matchRegex := regexp.MustCompile(targetKey)
 
 	labels := data.GetLabels()
@@ -30,45 +43,186 @@ func findAndReplaceValue(data unstructured.Unstructured, targetKey string, repla
 	}
 	data.SetLabels(labels)
 
+	return data
+}
+
+func findAndReplaceAnnotationsValue(data unstructured.Unstructured, targetKey string, replacement string) unstructured.Unstructured {
+	matchRegex := regexp.MustCompile(targetKey)
+
 	annotations := data.GetAnnotations()
-	if val, ok := labels["provider.cattle.io"]; ok && matchRegex.MatchString(val) {
+	if val, ok := annotations["management.cattle.io/current-cluster-controllers-version"]; ok && matchRegex.MatchString(val) {
 		annotations["management.cattle.io/current-cluster-controllers-version"] = matchRegex.ReplaceAllString(val, replacement)
 	}
 	data.SetAnnotations(annotations)
 
-	status, found, _ := unstructured.NestedFieldCopy(data.Object, "status")
+	return data
+}
+
+func findAndReplaceStatusValue(data unstructured.Unstructured, targetKey string, replacement string) unstructured.Unstructured {
+	matchRegex := regexp.MustCompile(targetKey)
+	objectData := data.UnstructuredContent()
+
+	status, found := objectData["status"].(map[string]interface{})
 	if found {
-		status.(map[string]interface{})["driver"] = "imported"
-		if matchRegex.MatchString(status.(map[string]string)["provider"]) {
-			status.(map[string]interface{})["provider"] = matchRegex.ReplaceAllString(status.(map[string]string)["provider"], replacement)
+		status["driver"] = "imported"
+		if matchRegex.MatchString(status["provider"].(string)) {
+			status["provider"] = matchRegex.ReplaceAllString(status["provider"].(string), replacement)
+		}
+	}
+	version := status["version"].(map[string]interface{})
+	currentPlatform := version["platform"]
+	version = map[string]interface{}{
+		"platform": currentPlatform,
+	}
+	status["version"] = version
+	objectData["status"] = status
+	data.SetUnstructuredContent(objectData)
+
+	return data
+}
+
+func findAndReplaceKeyOn(localCluster unstructured.Unstructured, target string, targetKey string, replacement string) unstructured.Unstructured {
+	targetObject, err := getTargetObject(localCluster.UnstructuredContent(), target)
+	if err != nil {
+		logrus.Fatalf("Something went wrong getting `%v`; %v", target, err)
+	}
+
+	targetObject = findAndReplaceKey(targetObject, targetKey, replacement)
+
+	return localCluster
+}
+
+// This is highly opinionated and designed for this tools' implementation.
+// DO NOT reuse this without understanding why it was made the way it is.
+func getTargetObject(cluster map[string]interface{}, target string) (map[string]any, error) {
+	if targetDepth := strings.Count(target, "."); targetDepth >= 1 {
+		targetParts := strings.Split(target, ".")
+		tempItem := cluster
+		for _, part := range targetParts {
+			newTempItem, err := getTargetObject(tempItem, part)
+			if err != nil {
+				return make(map[string]interface{}), err
+			}
+			tempItem = newTempItem
+		}
+
+		return tempItem, nil
+	}
+
+	targetObject := cluster[target]
+	if targetObject == nil {
+		return make(map[string]interface{}), fmt.Errorf("invalid target `%v` on resource", target)
+	}
+
+	return targetObject.(map[string]interface{}), nil
+}
+
+func findAndReplaceKey(data map[string]interface{}, targetKey string, replacement string) map[string]interface{} {
+	matchRegex := regexp.MustCompile(targetKey)
+
+	for key, value := range data {
+		if matchRegex.MatchString(key) {
+			newKey := matchRegex.ReplaceAllString(key, replacement)
+			delete(data, key)
+			data[newKey] = value
 		}
 	}
 
 	return data
 }
 
-func findAndReplaceKey(data unstructured.Unstructured, targetKey string, replacement string) unstructured.Unstructured {
+func findElement(array []metav1.ManagedFieldsEntry, search string) (*metav1.ManagedFieldsEntry, int) {
+	for id, element := range array {
+		if element.Manager == search { // check the condition if its true return index
+			return &element, id
+		}
+	}
+
+	return nil, 0
+}
+
+func findAndReplaceManagedFieldsKey(localCluster unstructured.Unstructured, targetKey string, replacement string) unstructured.Unstructured {
+	managedFields := localCluster.GetManagedFields()
+	rancherFields, _ := findElement(managedFields, "rancher")
+	if rancherFields == nil {
+		return localCluster
+	}
 	matchRegex := regexp.MustCompile(targetKey)
-	spec := data.UnstructuredContent()
-	managedFields := spec["managedFields"].([]map[string]interface{})[0]
-	for key, value := range managedFields {
-		if matchRegex.MatchString(key) {
-			newKey := matchRegex.ReplaceAllString(key, replacement)
-			delete(managedFields, key)
-			managedFields[newKey] = value
+
+	fields := rancherFields.FieldsV1
+	fieldsJson := fields.String()
+	fieldsJson = matchRegex.ReplaceAllString(fieldsJson, replacement)
+	err := fields.UnmarshalJSON([]byte(fieldsJson))
+	if err != nil {
+		return localCluster
+	}
+	localCluster.SetManagedFields(managedFields)
+
+	return localCluster
+}
+
+func findAndReplaceValueOn(localCluster unstructured.Unstructured, target string, targetKey string, replacement string) unstructured.Unstructured {
+	targetObject, err := getTargetObject(localCluster.UnstructuredContent(), target)
+	if err != nil {
+		logrus.Fatalf("Something went wrong getting `%v`; %v", target, err)
+	}
+
+	logrus.Infof("data out: %v", localCluster)
+	updatedObject := findAndReplaceValue(targetObject, targetKey, replacement)
+	logrus.Infof("updated out: %v", updatedObject)
+	err = setTargetObject(&localCluster, target, updatedObject)
+	if err != nil {
+		logrus.Fatalf("Something went wrong getting `%v`; %v", target, err)
+	}
+	logrus.Infof("data out: %v", localCluster)
+
+	return localCluster
+}
+
+func setTargetObject(cluster *unstructured.Unstructured, target string, object map[string]interface{}) error {
+	unstructuredObj := cluster.UnstructuredContent()
+	if targetDepth := strings.Count(target, "."); targetDepth >= 1 {
+		targetParts := strings.Split(target, ".")
+
+		// Traverse the map to reach the nested map where the update will happen
+		currentMap := unstructuredObj
+		for i, key := range targetParts {
+			// If it's the last key, update the value
+			if i == len(targetParts)-1 {
+				currentMap[key] = object
+				// Create a new unstructured object with the updated content
+				cluster.SetUnstructuredContent(unstructuredObj)
+				return nil
+			}
+
+			// Otherwise, move deeper into the map
+			if nextMap, ok := currentMap[key].(map[string]interface{}); ok {
+				currentMap = nextMap
+			} else {
+				// If the path does not exist, return an error
+				return fmt.Errorf("path does not exist: %s", target)
+			}
+		}
+		return nil
+	}
+
+	// non-nested update
+	unstructuredObj[target] = object
+	cluster.SetUnstructuredContent(unstructuredObj)
+
+	return nil
+}
+
+func findAndReplaceValue(data map[string]any, targetKey string, replacement string) map[string]interface{} {
+	matchRegex := regexp.MustCompile(targetKey)
+
+	for key, value := range data {
+		stringVal, ok := value.(string)
+		if ok && matchRegex.MatchString(stringVal) {
+			newVal := matchRegex.ReplaceAllString(stringVal, replacement)
+			data[key] = newVal
 		}
 	}
-	spec["managedFields"] = []interface{}{managedFields}
-
-	for key, value := range spec {
-		if matchRegex.MatchString(key) {
-			newKey := matchRegex.ReplaceAllString(key, replacement)
-			delete(spec, key)
-			spec[newKey] = value
-		}
-	}
-
-	data.SetUnstructuredContent(spec)
 
 	return data
 }
--- pkg/migrate/migrate_test.go
+++ pkg/migrate/migrate_test.go
@@ -2,6 +2,8 @@ package migrate
 
 import (
 	"embed"
+	"encoding/json"
+	"github.com/sirupsen/logrus"
 	"github.com/stretchr/testify/assert"
 	"k8s.io/apimachinery/pkg/apis/meta/v1/unstructured"
 	"testing"
@@ -10,74 +12,271 @@ import (
 //go:embed testdata/**/local.json
 var testfolder embed.FS
 
+func prepareUnstructred(input string) unstructured.Unstructured {
+	jsonData := make(map[string]interface{})
+	_ = json.Unmarshal([]byte(input), &jsonData)
+	testObj := unstructured.Unstructured{}
+	testObj.SetUnstructuredContent(jsonData)
+
+	return testObj
+}
+
 func TestK3sRancherToRke2Rancher(t *testing.T) {
 	tests := []struct {
 		name     string
-		input    map[string]interface{}
-		expected map[string]interface{}
+		newInput unstructured.Unstructured
+		input    string
+		expected string
 	}{
 		{
 			name: "Test 1",
-			input: map[string]interface{}{
-				"k3s": []interface{}{
-					"foo",
-					"bar",
-				},
-				"f:k3s": []interface{}{
-					[]interface{}{"baz"},
-					[]interface{}{"qux"},
-				},
-			},
-			expected: map[string]interface{}{
-				"rke2": []interface{}{
-					"foo",
-					"bar",
-				},
-				"f:rke2": []interface{}{
-					[]interface{}{"baz"},
-					[]interface{}{"qux"},
-				},
-			},
-		},
-		{
-			name: "Test 2",
-			input: map[string]interface{}{
-				"k3s": []interface{}{
-					[]interface{}{
-						map[string]interface{}{
-							"f:k3s": "foo",
-						},
-						map[string]interface{}{
-							"k3s":              []interface{}{"bar"},
-							"something-else":   "notk3sMATCHasdSOMEHASH",
-							"provider.rancher": "k3s",
-						},
-					},
-				},
-			},
-			expected: map[string]interface{}{
-				"rke2": []interface{}{
-					[]interface{}{
-						map[string]interface{}{
-							"f:rke2": "foo",
-						},
-						map[string]interface{}{
-							"rke2":             []interface{}{"bar"},
-							"something-else":   "notk3sMATCHasdSOMEHASH",
-							"provider.rancher": "rke2",
-						},
-					},
-				},
-			},
+			input: `{
+  "metadata": {
+    "annotations": {
+      "management.cattle.io/current-cluster-controllers-version": "v1.27.11+k3s1"
+    },
+    "labels": {
+      "provider.cattle.io": "k3s"
+    },
+    "managedFields": [
+      {
+        "apiVersion": "management.cattle.io/v3",
+        "fieldsType": "FieldsV1",
+        "fieldsV1": {
+          "f:spec": {
+            ".": {},
+            "f:k3sConfig": {
+              ".": {},
+              "f:k3supgradeStrategy": {
+                ".": {},
+                "f:serverConcurrency": {},
+                "f:workerConcurrency": {}
+              },
+              "f:kubernetesVersion": {}
+            }
+          }
+        },
+        "manager": "rancher",
+        "operation": "Update",
+        "time": "2024-05-02T13:47:19Z"
+      }
+    ]
+  },
+  "spec": {
+    "k3sConfig": {
+      "k3sUpgradeStrategy": {
+        "serverConcurrency": 1,
+        "workerConcurrency": 1
+      },
+      "kubernetesVersion": "v1.27.11+k3s1"
+    }
+  },
+  "status": {
+    "driver": "k3s",
+    "provider": "k3s",
+    "version": {
+      "buildDate": "2024-02-29T20:10:42Z",
+      "compiler": "gc",
+      "gitCommit": "06d6bc80b469a61e5e90438b1f2639cd136a89e7",
+      "gitTreeState": "clean",
+      "gitVersion": "v1.27.11+k3s1",
+      "goVersion": "go1.21.7",
+      "major": "1",
+      "minor": "27",
+      "platform": "linux/amd64"
+    }
+  }
+}`,
+			expected: `{
+  "metadata": {
+    "annotations": {
+      "management.cattle.io/current-cluster-controllers-version": "v1.27.11+rke2r1"
+    },
+    "labels": {
+      "provider.cattle.io": "rke2"
+    },
+    "managedFields": [
+      {
+        "apiVersion": "management.cattle.io/v3",
+        "fieldsType": "FieldsV1",
+        "fieldsV1": {
+          "f:spec": {
+            ".": {},
+            "f:rke2Config": {
+              ".": {},
+              "f:rke2upgradeStrategy": {
+                ".": {},
+                "f:serverConcurrency": {},
+                "f:workerConcurrency": {}
+              },
+              "f:kubernetesVersion": {}
+            }
+          }
+        },
+        "manager": "rancher",
+        "operation": "Update",
+        "time": "2024-05-02T13:47:19Z"
+      }
+    ]
+  },
+  "spec": {
+    "rke2Config": {
+      "rke2UpgradeStrategy": {
+        "serverConcurrency": 1,
+        "workerConcurrency": 1
+      },
+      "kubernetesVersion": "v1.27.11+rke2r1"
+    }
+  },
+  "status": {
+    "driver": "imported",
+    "provider": "rke2",
+    "version": {
+      "platform": "linux/amd64"
+    }
+  }
+}`,
 		},
 	}
 
 	for _, test := range tests {
 		t.Run(test.name, func(t *testing.T) {
-			testObj := unstructured.Unstructured{}
-			testObj.SetUnstructuredContent(test.input)
+			testObj := prepareUnstructred(test.input)
+			logrus.Infof("Test obj before: %v", testObj)
 			actual := K3sRancherToRke2Rancher(testObj)
-			assert.Equal(t, test.expected, actual.UnstructuredContent())
+			logrus.Infof("Test obj after: %v", actual)
+			expected := prepareUnstructred(test.expected)
+			assert.Equal(t, expected, actual)
+		})
+	}
+}
+
+func TestRke2RancherToK3sRancher(t *testing.T) {
+	tests := []struct {
+		name     string
+		newInput unstructured.Unstructured
+		input    string
+		expected string
+	}{
+		{
+			name: "Test 1",
+			input: `{
+  "metadata": {
+    "annotations": {
+      "management.cattle.io/current-cluster-controllers-version": "v1.27.11+rke2r1"
+    },
+    "labels": {
+      "provider.cattle.io": "rke2"
+    },
+    "managedFields": [
+      {
+        "apiVersion": "management.cattle.io/v3",
+        "fieldsType": "FieldsV1",
+        "fieldsV1": {
+          "f:spec": {
+            ".": {},
+            "f:rke2Config": {
+              ".": {},
+              "f:rke2upgradeStrategy": {
+                ".": {},
+                "f:serverConcurrency": {},
+                "f:workerConcurrency": {}
+              },
+              "f:kubernetesVersion": {}
+            }
+          }
+        },
+        "manager": "rancher",
+        "operation": "Update",
+        "time": "2024-05-02T13:47:19Z"
+      }
+    ]
+  },
+  "spec": {
+    "rke2Config": {
+      "rke2UpgradeStrategy": {
+        "serverConcurrency": 1,
+        "workerConcurrency": 1
+      },
+      "kubernetesVersion": "v1.27.11+rke2r1"
+    }
+  },
+  "status": {
+    "driver": "rke2",
+    "provider": "rke2",
+    "version": {
+      "buildDate": "2024-02-29T20:10:42Z",
+      "compiler": "gc",
+      "gitCommit": "06d6bc80b469a61e5e90438b1f2639cd136a89e7",
+      "gitTreeState": "clean",
+      "gitVersion": "v1.27.11+rke2r1",
+      "goVersion": "go1.21.7",
+      "major": "1",
+      "minor": "27",
+      "platform": "linux/amd64"
+    }
+  }
+}`,
+			expected: `{
+  "metadata": {
+    "annotations": {
+      "management.cattle.io/current-cluster-controllers-version": "v1.27.11+k3s1"
+    },
+    "labels": {
+      "provider.cattle.io": "k3s"
+    },
+    "managedFields": [
+      {
+        "apiVersion": "management.cattle.io/v3",
+        "fieldsType": "FieldsV1",
+        "fieldsV1": {
+          "f:spec": {
+            ".": {},
+            "f:k3sConfig": {
+              ".": {},
+              "f:k3supgradeStrategy": {
+                ".": {},
+                "f:serverConcurrency": {},
+                "f:workerConcurrency": {}
+              },
+              "f:kubernetesVersion": {}
+            }
+          }
+        },
+        "manager": "rancher",
+        "operation": "Update",
+        "time": "2024-05-02T13:47:19Z"
+      }
+    ]
+  },
+  "spec": {
+    "k3sConfig": {
+      "k3sUpgradeStrategy": {
+        "serverConcurrency": 1,
+        "workerConcurrency": 1
+      },
+      "kubernetesVersion": "v1.27.11+k3s1"
+    }
+  },
+  "status": {
+    "driver": "imported",
+    "provider": "k3s",
+    "version": {
+      "platform": "linux/amd64"
+    }
+  }
+}`,
+		},
+	}
+
+	for _, test := range tests {
+		t.Run(test.name, func(t *testing.T) {
+			testObj := prepareUnstructred(test.input)
+			logrus.Infof("Test obj before: %v", testObj)
+			actual := Rke2RancherToK3sRancher(testObj)
+			logrus.Infof("Test obj after: %v", actual)
+			expected := prepareUnstructred(test.expected)
+			assert.Equal(t, expected, actual)
 		})
 	}
 }
